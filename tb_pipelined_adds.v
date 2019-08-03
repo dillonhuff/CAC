503df5183add0a0337817816cd4e565c32ef862d
Dillon Huff <dillonhuff@gmail.com>
2019-08-02 21:40:02 -0700

    Now have pipelined adder

--- tb_pipelined_adds.v
+++ tb_pipelined_adds.v
@@ -34,20 +34,25 @@ module test();
       #1 in = 15;
       $display("result = %d", result);
 
-      `assert(result, 12);
+      `assert(result, 12)
       
 
       #1 clk = 0;
       #1 clk = 1;
       #1 clk = 0;
 
-
       $display("result = %d", result);
 
-      `assert(result, 17);      
+      `assert(result, 17)
       #1 valid = 0;
+      #1 in = 18;      
       
-      
+      #1 clk = 0;
+      #1 clk = 1;
+      #1 clk = 0;
+
+      $display("invalid result = %d", result);
+      `assert(result, 17)
    end
 
    pipelined_adds dut(.clk(clk), .rst(rst), .result(result), .in_valid(valid), .in_data(in));
